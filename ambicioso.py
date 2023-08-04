b6a62e4aee47b0b51d481ad2219076db03faa465
Aaron Albrecht <141296417+AaronProgramas@users.noreply.github.com>
2023-08-04 15:38:03 -0300

    APP now plots values for RGB, HSI, HSL, CMYK and gray scale.

--- ambicioso.py
+++ ambicioso.py
@@ -4,23 +4,37 @@ from PIL import Image, ImageTk, ImageDraw
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from colorsys import rgb_to_hsv, rgb_to_hls
 
 selected_areas = []
 image = None
 
-def extract_rgb_values_from_area(selected_area):
+def extract_color_values_from_area(selected_area):
     pixels = selected_area.getdata()
-    rgb_values = [pixel[:3] for pixel in pixels]
-    return rgb_values
+    color_values = []
+    for pixel in pixels:
+        rgb = pixel[:3]
+        hsi = rgb_to_hsv(*rgb)
+        hsl = rgb_to_hls(*rgb)
+        cmyk = rgb_to_cmyk(*rgb)
+        gray = rgb_to_gray(*rgb)
+        color_values.append((*rgb, *hsi, *hsl, *cmyk, gray))
+    return color_values
 
-def extract_rgb_values(image, x, y, radius=10):
-    rgb_values = []
-    for i in range(x - radius, x + radius + 1):
-        for j in range(y - radius, y + radius + 1):
-            if 0 <= i < image.width and 0 <= j < image.height:
-                pixel = image.getpixel((i, j))
-                rgb_values.append(pixel[:3])
-    return rgb_values
+def rgb_to_cmyk(r, g, b):
+    c = 1 - (r / 255)
+    m = 1 - (g / 255)
+    y = 1 - (b / 255)
+    k = min(c, m, y)
+    if k == 1:
+        return 0, 0, 0, 1
+    c = (c - k) / (1 - k)
+    m = (m - k) / (1 - k)
+    y = (y - k) / (1 - k)
+    return c, m, y, k
+
+def rgb_to_gray(r, g, b):
+    return 0.2989 * r + 0.587 * g + 0.114 * b
 
 def select_image():
     global image
@@ -38,9 +52,9 @@ def display_image(image):
         x, y = event.x, event.y
         selected_area = image.crop((x - 10, y - 10, x + 10, y + 10))
         draw_red_circle(x, y)
-        rgb_values = extract_rgb_values(image, x, y, radius=10)
-        selected_areas.append((selected_area, rgb_values))
-        plot_area_histogram(rgb_values)
+        color_values = extract_color_values_from_area(selected_area)
+        selected_areas.append((selected_area, color_values))
+        plot_area_histogram(color_values)
 
     canvas.bind("<Button-1>", on_click)
 
@@ -49,16 +63,22 @@ def draw_red_circle(x, y):
     x2, y2 = x + 10, y + 10
     canvas.create_oval(x1, y1, x2, y2, outline="red")
 
-def show_rgb_sheet():
+def show_color_sheet():
     if not selected_areas:
         messagebox.showinfo("No Areas Selected", "Please select areas on the image first.")
         return
 
     data = []
-    for idx, (selected_area, rgb_values) in enumerate(selected_areas, 1):
-        data.extend([[f"Selected Area {idx}", *rgb] for rgb in rgb_values])
+    for idx, (_, color_values) in enumerate(selected_areas, 1):
+        data.extend([[f"Selected Area {idx}", *color] for color in color_values])
 
-    df = pd.DataFrame(data, columns=["Area", "R", "G", "B"])
+    df = pd.DataFrame(data, columns=[
+        "Area", "R", "G", "B",
+        "Hue", "Saturation (HSI)", "Value", 
+        "Hue", "Saturation (HSL)", "Lightness",
+        "Cyan", "Magenta", "Yellow", "Black (CMYK)",
+        "Grayscale"
+    ])
     display_sheet(df)
 
 def save_to_text_file(df):
@@ -74,15 +94,12 @@ def save_to_excel_file(df):
 
 def display_sheet(df):
     top = tk.Toplevel(root)
-    top.title("RGB Values Sheet")
+    top.title("Color Values Sheet")
 
     table = tk.Text(top, wrap=tk.NONE)
     table.pack()
 
-    # Create a table-like string for displaying in the Text widget
     table_str = df.to_string(index=False)
-
-    # Insert the table into the Text widget
     table.insert(tk.END, table_str)
 
     save_text_button = tk.Button(top, text="Save to Text File", command=lambda: save_to_text_file(df))
@@ -96,22 +113,26 @@ def plot_histogram():
         messagebox.showinfo("No Areas Selected", "Please select areas on the image first.")
         return
 
-def plot_area_histogram(rgb_values):
-    plt.figure()
-    np_rgb_values = np.array(rgb_values)
-    plt.hist(np_rgb_values[:, 0], bins=256, color='r', histtype='step', alpha=0.7, label="R")
-    plt.hist(np_rgb_values[:, 1], bins=256, color='g', histtype='step', alpha=0.7, label="G")
-    plt.hist(np_rgb_values[:, 2], bins=256, color='b', histtype='step', alpha=0.7, label="B")
-    plt.xlabel('color value')
-    plt.ylabel('number of pixels')
-    plt.legend()
+def plot_area_histogram(color_values):
+    plt.figure(figsize=(12, 8))
+    np_color_values = np.array(color_values)
+    
+    for i, label in enumerate(["R", "G", "B", "Hue (HSI)", "Saturation (HSI)", "Value", 
+                               "Hue (HSL)", "Saturation (HSL)", "Lightness", 
+                               "Cyan (CMYK)", "Magenta (CMYK)", "Yellow (CMYK)", "Black (CMYK)", "Grayscale"]):
+        plt.subplot(3, 5, i + 1)
+        plt.hist(np_color_values[:, i], bins=256, histtype='step', alpha=0.7)
+        plt.xlabel(label)
+        plt.ylabel('number of pixels')
+    
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Professor Aaron's Magic RGB Extractor")
+    root.title("Professor Aaron's Magical Color Analyzer")
 
-    header_label = tk.Label(root, text="Select an image to extract RGB values:")
+    header_label = tk.Label(root, text="Select an image to extract color values:")
     header_label.pack(pady=10)
 
     select_button = tk.Button(root, text="Select Image", command=select_image)
@@ -120,10 +141,10 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root, bg="white")
     canvas.pack()
 
-    sheet_button = tk.Button(root, text="Show RGB Values Sheet", command=show_rgb_sheet)
+    sheet_button = tk.Button(root, text="Show Color Values Sheet", command=show_color_sheet)
     sheet_button.pack(pady=5)
 
-    histogram_button = tk.Button(root, text="Plot Histogram", command=plot_histogram)
+    histogram_button = tk.Button(root, text="Plot Color Histogram", command=plot_histogram)
     histogram_button.pack(pady=5)
 
     root.mainloop()
